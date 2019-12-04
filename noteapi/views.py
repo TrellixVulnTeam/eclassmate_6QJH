800a90111a253e737f92ac356bc567a83ca4bf3f
Saman shrestha <samanshrestha80@gmail.com>
2019-12-04 19:16:22 +0545

    Serializers.py has been added with fully running with basic authentication

--- noteapi/views.py
+++ noteapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Note
+from .serializers import NoteSerializers
+
+class NoteSerializerViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializers
+    queryset = Note.objects.all()
